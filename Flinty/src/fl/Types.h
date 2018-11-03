4afbc807c4474a48502effa5dd8d474e0a51aeae
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-11-03 19:00:34 +0100

    Added more type aliases

--- Flinty/src/fl/Types.h
+++ Flinty/src/fl/Types.h
@@ -1,4 +1,23 @@
 #pragma once
 
 typedef unsigned int uint;
-typedef unsigned char byte;
+
+typedef char int8;
+typedef unsigned char uint8;
+
+typedef short int16;
+typedef unsigned short uint16;
+
+typedef long int32;
+typedef unsigned long uint32;
+
+typedef long long int64;
+typedef unsigned long long uint64;
+
+typedef float float32;
+typedef double float64;
+
+typedef uint8 byte;
+typedef uint16 word;
+typedef uint32 dword;
+typedef uint64 qword;
